d7cdfa6f6a488140d4828da0389b901cc75ab72b
ElenaDias <elenadias@ymail.com>
2023-02-22 16:14:57 +0100

    dichiarata la funzione di onClick in un punto a parte

--- src/components/SectionButton/SectionButton.js
+++ src/components/SectionButton/SectionButton.js
@@ -1,13 +1,14 @@
 import './SectionButton.css'
 const SectionButton = ({linkHref, title}) => {
+    const handleClick = (e) => {
+        e.preventDefault();
+        window.location.href=linkHref;
+        }
     return(
         <>
             {/* <a href={linkHref} className="sectionButton"> {title} </a> */}
 
-            <button className="sectionButton" onClick={ (e) => {
-                e.preventDefault();
-                window.location.href=linkHref;
-                }}
+            <button className="sectionButton" onClick={handleClick}
             >{ title }</button>
 
         </>
