16be696fae3f6f7c4044ce1e79ef429e1ca19b20
eletodosdias <71981440+eletodosdias@users.noreply.github.com>
2023-02-28 23:19:03 +0100

    ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,9 +3,9 @@ const ContactForm = () => {
     return(
         <form>
             <input className="form-control" type="text" placeholder="Inserisci il tuo nome"/>
-            <input className="form-control" type="email"   placeholder="Inserisci il tuo inidirizo email"/>
-            <input className="form-control" type="number"  placeholder="Inserisci il tuo numero" />
-            <textarea  className="form-control" placeholder="Inserisci messaggio" ></textarea>
+            <input className="form-control" type="email" placeholder="Inserisci il tuo inidirizo email"/>
+            <input className="form-control" type="number" placeholder="Inserisci il tuo numero" />
+            <textarea className="form-control" placeholder="Inserisci messaggio" ></textarea>
             <input className='submit' type="button" value="Invia" />
         </form>
     )
